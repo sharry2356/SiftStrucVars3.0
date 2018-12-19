b3cb40e446f5497d59286670874d24883d5e571f
shamirharry <sch29529@uga.edu>
2018-12-18 23:06:05 -0500

    add intermediates wc -l, and deleted actual intermediate files as they are made along the pipeline

--- NEWsiftSVMaster.sh
+++ NEWsiftSVMaster.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#instead of doing intermediates at the end do it along the way and delete intermediates files along the way too (>> to an appended intermedial wc -l list) !!!
-	# wc -l outputs and intermediates; rm those intermediate files; cp  wc-l doc down?? or maybe just leave one on each level?? try the latter first  
-	# make sure to get max_SVlength values in there too (it is in an outside folder from major large loop)  
-	# if you do it before mkdir then you have to mv it into that dir otherwise it will be overwritten 
- 
+# make sure outupts are correct 
+# create inputs for files on this bash script 
+# delete unnecesary files 
 
 correlations=(0.95 0.975 0.99)
 max_SVlengths=(5e6 1e6 500000 1e5)
@@ -39,7 +37,7 @@ do
   grep ID=mRNA GOIs_and_Cs_annotatedgenome.bed > GOIs_and_Cs_mRNA.bed 
   grep ID=CDS GOIs_and_Cs_annotatedgenome.bed > GOIs_and_Cs_CDS.bed
   grep ID=five_prime_UTR GOIs_and_Cs_annotatedgenome.bed > GOIs_and_Cs_five_prime_UTR.bed
-  ####HERE####
+  wc -l Correlation_Table *.* > wc_l_corr.txt
   for max_SVlength in ${max_SVlengths[*]}
   do	
     # **** Find the CDS that intersect SVs and then concatonate with full set of CDS of those genes/mRNAs
@@ -60,8 +58,9 @@ do
     grep -F -f SVs_intersect_CDS_genelist.txt ../Meristems.txt >> Meristems_CDS.txt
     Rscript ../Rscripts/siftSVsCDSRelExpr.R $TM_SIM_FM $TM_SIM_FM_over_other_meristem_ratio $Anthesis_fiveDPA_tenDPA_psc $A510psc_over_A510_NONpsc $A510psc_over_NON_A510_psc $fiveDPA_psc_over_A_psc_and_tenDPA_psc
     ###HERE### Use this to include the SV wc -l from the outside folder and don't forget to move into mkdir 
+    wc -l ../${max_SVlength}maxSV/bedformat_SVs.bed GOIsandCs_CDS_that_intersect_SVs.bed genes_with_CDS_overlapped_by_SVs.txt Full_CDS_sets.bed overlapping_SVs_and_GOIsandCs_CDS.bed SVs_no_intersect_CDS.bed SVs_no_interesct_CDS_or_5_prime.bed SVs_only_intersect_fives.bed SVs_intersect_CDS.txt SVs_intersect_CDS_genelist.txt Relevent_Expression_SVs_intersect_CDS.txt Anthesis_CDS.txt Meristems_CDS.txt > wc_l_SV.txt 
     mkdir -p ${max_SVlength}maxSV
-    mv -t ${max_SVlength}maxSV GOIsandCs_CDS_that_intersect_SVs.bed genes_with_CDS_overlapped_by_SVs.txt Full_CDS_sets.bed overlapping_SVs_and_GOIsandCs_CDS.bed       SVs_no_intersect_CDS.bed SVs_no_interesct_CDS_or_5_prime.bed SVs_intersect_CDS.txt SVs_intersect_CDS_genelist.txt SVs_only_intersect_fives.bed Relevent_Expression_SVs_intersect_CDS.txt
+    mv -t ${max_SVlength}maxSV SVs_no_interesct_CDS_or_5_prime.bed SVs_only_intersect_fives.bed Relevent_Expression_SVs_intersect_CDS.txt SVs_intersect_CDS.txt wc_l_SV.txt 
     cd ${max_SVlength}maxSV
     for windowSize in ${fivePrimeWindows[*]}
     do
@@ -73,10 +72,17 @@ do
       grep -F -f SVs_intersect_RE_genelist.txt ../../Meristems.txt >> Meristems_RE.txt
       Rscript ../../Rscripts/siftSVsRERelExpr.R $TM_SIM_FM $TM_SIM_FM_over_other_meristem_ratio $Anthesis_fiveDPA_tenDPA_psc $A510psc_over_A510_NONpsc $A510psc_over_NON_A510_psc $fiveDPA_psc_over_A_psc_and_tenDPA_psc
       ###HERE### don't forget to move into mkdir 
+      wc -l SVs_upstream_of_genes.bed SVs_intersect_RE.txt SVs_intersect_RE_genelist.txt Relevent_Expression_SVs_intersect_RE.txt Anthesis_RE.txt Meristems_RE.txt > wc_l_fiveprimewind.txt
       mkdir -p ${windowSize}fiveprimewindow
-      mv -t ${windowSize}fiveprimewindow SVs_upstream_of_genes.bed SVs_intersect_RE.txt SVs_intersect_RE_genelist.txt Relevent_Expression_SVs_intersect_RE.txt     
+      mv -t ${windowSize}fiveprimewindow SVs_intersect_RE.txt Relevent_Expression_SVs_intersect_RE.txt wc_l_fiveprimewind.txt    
     done 
+    rm SVs_no_interesct_CDS_or_5_prime.bed SVs_only_intersect_fives.bed SVs_upstream_of_genes.bed SVs_intersect_RE_genelist.txt Anthesis_RE.txt Meristems_RE.txt 
     cd .. 
   done 
+  rm GOIsandCs_CDS_that_intersect_SVs.bed genes_with_CDS_overlapped_by_SVs.txt Full_CDS_sets.bed overlapping_SVs_and_GOIsandCs_CDS.bed SVs_no_intersect_CDS.bed SVs_intersect_CDS_genelist.txt Anthesis_CDS.txt Meristems_CDS.txt
+  rm GOIs_and_Correlatedgenes_list.txt GOIs_and_Cs_annotatedgenome.bed GOIs_and_Cs_mRNA.bed GOIs_and_Cs_CDS.bed GOIs_and_Cs_five_prime_UTR.bed
   cd .. 
 done
+
+# rm maxSV and annotated genome intermediate files in 1st for loop 
+rm -r *maxSV annotatedgenomeinter.txt annotatedgenome.gff
