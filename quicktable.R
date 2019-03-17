c15a5d642d64ccca993132eef00b3c9b565188f6
shamirharry <sch29529@uga.edu>
2019-03-17 19:48:00 -0400

    automate condensed table formation and updates it so it can be used when given a directory with all output correlation folders

--- quicktable.R
+++ quicktable.R
@@ -26,8 +26,8 @@ condensedtab <- function(genesv,corrtable,SVs_intersect_CDS_file,SVs_intersect_R
   corrtable_file<-"Correlation_Table"
   SVs_intersect_CDS_file<-"1e6maxSV/SVs_intersect_CDS.txt"
   SVs_intersect_RE_file<-"1e6maxSV/5000fiveprimewindow/SVs_intersect_RE.txt"
-  humtab_file<-"../../../Documents/humtab"
-  Rel_Expr_file<-"../Anthesis.txt"
+  humtab_file<-"../../humtab"
+  Rel_Expr_file<-"../../../Desktop/SiftStrucVars3.0/Anthesis.txt"
   humtab<-read.delim(humtab_file)
   genesv<-as.character(humtab$genesv)
   corrtable<-read.delim(corrtable_file)
@@ -35,7 +35,7 @@ condensedtab <- function(genesv,corrtable,SVs_intersect_CDS_file,SVs_intersect_R
   SV_intersect_CDS<-read.delim(SVs_intersect_CDS_file)
   SV_intersect_CDS<-SV_intersect_CDS[grep("OVATE",SV_intersect_CDS[,13]),c(1:5,13)]
   SV_intersect_CDS<-cbind(SV_intersect_CDS, intersect_CDS = rep("yes",nrow(SV_intersect_CDS)))
-  if (SVs_intersect_RE == 0) {
+  if (SVs_intersect_RE_file == 0) {
     SV_inter<-SV_intersect_CDS
   } else {
     SV_intersect_RE<-read.delim(SVs_intersect_RE_file)
@@ -52,7 +52,6 @@ condensedtab <- function(genesv,corrtable,SVs_intersect_CDS_file,SVs_intersect_R
   Anthesis<-read.delim(Rel_Expr_file)
   final<-c()
   for (r in 1:nrow(SV_inter)) {
-    #gois <- paste(corrtable[grep(SV_inter[r,4],corrtable$correlated_gene),1], collapse = ",")
     gois <- corrtable[grep(SV_inter[r,4],corrtable$correlated_gene),1]
     goiHumNames<-c()
     for (goi in gois) {
@@ -68,7 +67,8 @@ condensedtab <- function(genesv,corrtable,SVs_intersect_CDS_file,SVs_intersect_R
 
 write.table(final,"../../../Downloads/corr99_50000wind_1e6maxSV_OVATEonly.txt",row.names = F,sep = "\t")
 
-
+final<-final[!(grepl("DEL",final$svtype) & grepl("\\+",final$intersect_CDS)),]
+write.table(final,"../corr0975withplusdels",row.names = F,sep = "\t")
 
 
 
@@ -110,10 +110,60 @@ for (r in 1:nrow(group)) {
   }
 }
 
-#####
-group<-group3599
-groupnew<-c()
-for (r in 1:nrow(group)) {
-  protein <- relexpr[grep(group[r,]$affected_gene,relexpr$gene),]$Protein
-  groupnew <- rbind(cbind(group[r,],protein),groupnew)
+##### Making automated condensed table script 
+# use it when put all output folders (corrs) in same directory (theoretically can be used in SiftStrucVars3.0 itself)
+# do not have genes repeated (if greater than 0.99 don't have it in 0.95 or 0.975 etc.)
+# just setwd() to appropriate directory 
+corrs<-sort(list.dirs()[grep("corr$",list.dirs())],decreasing = T) 
+library(stringr)
+corrsnumbs<-str_extract(corrs,"(?<=\\./).+(?=c)")
+detach(package:stringr)
+final<-c()
+for (i in 1:length(corrs)) {
+  setwd(corrs[i])  
+  corrtable_file<-"Correlation_Table"
+  SVs_intersect_CDS_file<-"1e6maxSV/SVs_intersect_CDS.txt"
+  SVs_intersect_RE_file<-"1e6maxSV/5000fiveprimewindow/SVs_intersect_RE.txt"
+  humtab_file<-"../../humtab"
+  Rel_Expr_file<-"../../../Desktop/SiftStrucVars3.0/Anthesis.txt"
+  humtab<-read.delim(humtab_file)
+  genesv<-as.character(humtab$genesv)
+  corrtable<-read.delim(corrtable_file)
+  if (i > 1) { 
+     corrtable<-corrtable[corrtable$correlation <= corrsnumbs[(i-1)],]
+  }
+  corrtable<-corrtable[grep(paste(genesv,collapse = "|"),corrtable$GOI),]
+  SV_intersect_CDS<-read.delim(SVs_intersect_CDS_file)
+  SV_intersect_CDS<-SV_intersect_CDS[grep("OVATE",SV_intersect_CDS[,13]),c(1:5,13)]
+  SV_intersect_CDS<-cbind(SV_intersect_CDS, intersect_CDS = rep("yes",nrow(SV_intersect_CDS)))
+  # fix like try and except in python 
+  if (SVs_intersect_RE_file == 0) {
+    SV_inter<-SV_intersect_CDS
+  } else {
+    SV_intersect_RE<-read.delim(SVs_intersect_RE_file)
+    SV_intersect_RE<-SV_intersect_RE[grep("OVATE",SV_intersect_RE[,7]),]
+    intersect_CDS<-gsub("Yes","No in 5'UTR",SV_intersect_RE$intersect_5_prime)
+    SV_intersect_RE<-cbind(SV_intersect_RE[,c(1:5,7)], intersect_CDS)
+    SV_inter<-rbind(SV_intersect_CDS,SV_intersect_RE)
+  } 
+  grep1<-grep(paste(corrtable$correlated_gene[1:2538],collapse = "|"),SV_inter$affected_gene)
+  grep2<- grep(paste(corrtable$correlated_gene[2539:4500],collapse = "|"),SV_inter$affected_gene) 
+  grep3<- grep(paste(corrtable$correlated_gene[4501:6500],collapse = "|"),SV_inter$affected_gene) 
+  grep4<- grep(paste(corrtable$correlated_gene[6501:8500],collapse = "|"),SV_inter$affected_gene) 
+  SV_inter<-SV_inter[unique(c(grep1,grep2,grep3,grep4)),]
+  Anthesis<-read.delim(Rel_Expr_file)
+  onecorr<-c()
+  for (r in 1:nrow(SV_inter)) {
+    gois <- corrtable[grep(SV_inter[r,4],corrtable$correlated_gene),c("GOI","correlation")]
+    goiHumNames<-c()
+    for (it in 1:nrow(gois)) {
+      goiHumName<-paste(gois[it,c("GOI")],"(",humtab[grep(gois[it,c("GOI")],humtab$genesv),2],")","[",gois[it,c("correlation")],"]",sep = "")
+      goiHumNames<-append(goiHumNames,goiHumName)
+    }
+    protein<- as.character(Anthesis[grep(SV_inter[r,4],Anthesis$gene.name),c("description")])
+    onecorr <- rbind(cbind(gois = paste(goiHumNames,collapse = ","),SV_inter[r,],protein),onecorr)
+  }
+  onecorr <-cbind("corrthresh" = rep(corrsnumbs[i]),onecorr[,1:7],"sv_size_(bp)" = onecorr$V3-onecorr$V2,onecorr[,8:9])
+  final<-rbind(onecorr,final)
+  setwd("../")
 }
